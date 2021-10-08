b677644b78739b7f987ea35f86cf4ba12f9fd434
Sebastian <sebastianfox81@gmail.com>
2021-10-08 16:40:00 -0400

    Added gitignore file

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const http = require('http');
 
 const requestHandler = function(req, res) {
   res.setHeader('Content-Type', 'text/html');
-  
 };
 
 const server = http.createServer(requestHandler);
 
 server.listen(3000);
+
+console.log('hello')
