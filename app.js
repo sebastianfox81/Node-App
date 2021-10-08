62fa89812face3e0ff22b8fc394aa909079ac5f3
Sebastian <sebastianfox81@gmail.com>
2021-10-08 17:17:45 -0400

    Added '/add-product' route and create a form

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!')
-  next();
+
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next) => {
-  console.log('In another middleware!')
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello from Express</h1>')
 })
 
-const server = http.createServer(app);
+app.listen(3000);
 
-server.listen(3000);
