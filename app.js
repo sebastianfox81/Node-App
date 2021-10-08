2779ee1afe41b3606574ddfb63378d00105c7c17
Sebastian <sebastianfox81@gmail.com>
2021-10-08 17:01:32 -0400

    Implement app.use and res.send to send a response to the browser

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const express = require('express');
 
 const app = express();
 
-const requestHandler = function(req, res) {
-  res.setHeader('Content-Type', 'text/html');
-};
+app.use((req, res, next) => {
+  console.log('In the middleware!')
+  next();
+})
 
-const server = http.createServer(requestHandler);
+app.use((req, res, next) => {
+  console.log('In another middleware!')
+  res.send('<h1>Hello from Express</h1>')
+})
+
+const server = http.createServer(app);
 
 server.listen(3000);
-
-console.log('listening on port 3000')
