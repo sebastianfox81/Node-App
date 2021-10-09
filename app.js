eab590949e8ef30c1fdcf582c882f6fa32370ad8
Sebastian <sebastianfox81@gmail.com>
2021-10-09 15:54:35 -0400

    Added file route and method in shop.js

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const adminRoutes = require('./routes/admin')
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(adminRoutes)
 
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express</h1>')
-})
 
 app.listen(3000);
 
