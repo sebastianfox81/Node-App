7b8dd64198c8e828f876f8e3f555798bd4e80555
Sebastian <sebastianfox81@gmail.com>
2021-10-09 15:57:14 -0400

    Exported shop.js into app.js and passed in handler accordingly

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const adminRoutes = require('./routes/admin')
+const adminRoutes = require('./routes/admin');
+const shopRouts = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes)
+app.use(adminRoutes);
+app.use(adminRoutes);
 
 
 
