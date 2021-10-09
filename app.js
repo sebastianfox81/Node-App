1f8e8195c0f7582f8b8ed85e29cb7c955b0ad2df
Sebastian <sebastianfox81@gmail.com>
2021-10-09 15:47:03 -0400

    Setup admin file with routers

--- app.js
+++ app.js
@@ -6,14 +6,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
 
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/')
-})
 
 app.use('/', (req, res, next) => {
   res.send('<h1>Hello from Express</h1>')
