e99a470ccf4f2c0e0620656ec9de2c89f482b7ad
Sebastian <sebastianfox81@gmail.com>
2021-10-09 15:39:39 -0400

    Added body-parser package and stubbed out functionality

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
   res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
