69149ff54dc83879e86effbeb740cc8f7a46cac8
Sebastian <sebastianfox81@gmail.com>
2021-10-09 16:05:54 -0400

    Added 404 status in app.js

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const adminRoutes = require('./routes/admin');
-const shopRouts = require('./routes/shop');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(adminRoutes);
-app.use(adminRoutes);
+app.use(shopRoutes);
 
-
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page Not Found</h1>')
+})
 
 app.listen(3000);
 
