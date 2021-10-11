7b3b428a58ebb0424327e9d6e3c42d5c4a713f3d
Sebastian <sebastianfox81@gmail.com>
2021-10-11 16:03:44 -0400

    Added path module to shop.js and modified get request to sendFile with path.join

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path')
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express</h1>')
+  res.sendFile(path.join(__dirname, 'views', 'shop.html')) // path.join() detects the OS you are running and automatically constructs the path accordingly
 })
 module.exports = router
