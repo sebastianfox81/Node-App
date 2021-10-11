04fc3c24c06a29e81ab9be302b8b4eff825bedf5
Sebastian <sebastianfox81@gmail.com>
2021-10-11 16:06:14 -0400

    Added ../ to path.join

--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,6 @@ const path = require('path')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'views', 'shop.html')) // path.join() detects the OS you are running and automatically constructs the path accordingly
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')) // path.join() detects the OS you are running and automatically constructs the path accordingly
 })
 module.exports = router
